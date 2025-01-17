3235ff596f80bf3d7a6b2d1d3ee888e67ddbdfd4
YooSeungJun <12000ha@naver.com>
2025-01-17 20:11:46 +0900

    [Baekjoon-10773]: 제로

---
+++ seungjun/Baekjoon/Solution10773.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution10773 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int K = input.nextInt();
+        Stack<Integer> stacks = new Stack<>();
+
+        for (int k=0; k<K; k++) {
+            int num = input.nextInt();
+            if (num == 0) {
+                stacks.pop();
+                continue;
+            }
+            stacks.add(num);
+        }
+        if (stacks.isEmpty()) {
+            System.out.println(0);
+        }
+        else {
+            int answer = 0;
+            for (int s : stacks) {
+                answer += s;
+            }
+            System.out.println(answer);
+        }
+    }
+}
