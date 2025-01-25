38d0136175391e7236b230da311418c3fdfd671e
Grayson1999 <bbt1250912@gmail.com>
2025-01-26 01:13:46 +0900

    [Baekjoon-2798]: 블랙잭

---
+++ seungcheol/Baekjoon/Sol2798.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Sol2798 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] cards = new int[n];
+
+        for (int i = 0; i < n; i++){
+            cards[i] = sc.nextInt();
+        }
+        int max = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                for(int k = j+1; k< n; k++){
+                    int currentSum = cards[i]+cards[j]+cards[k];
+                    if(currentSum > max && currentSum <= m){
+                        max = currentSum;
+                    }
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+}
