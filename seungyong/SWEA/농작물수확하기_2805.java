1d81098fe0dc18b052ffd266c11d3712f7cef39c
sso9594 <sso9594@naver.com>
2025-01-27 00:19:44 +0900

    [SWEA-2805]: 농작물 수확하기

---
+++ seungyong/SWEA/농작물수확하기_2805.java
@@ -0,0 +1,44 @@
+package seungyong.SWEA;
+
+import java.util.Scanner;
+
+public class 농작물수확하기_2805 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for(int test_case=1; test_case <= T; test_case++){
+            int N = sc.nextInt();
+            int[][] arr = new int[N][N];
+            int sum = 0;
+
+            for (int i = 0; i < N; i++) {
+                String token = sc.next();
+                for (int j = 0; j < N; j++) {
+                    arr[i][j] = Integer.parseInt(token.charAt(j)+"");
+                }
+            }
+            
+            for (int i = 0; i < (N/2)+1; i++) {
+                for (int j = (N/2)+1; j <= (N/2)+i; j++) {
+                    sum += arr[i][j];
+                }
+                for (int j = (N/2); j >= (N/2)-i; j--) {
+                    sum += arr[i][j];
+                }
+            }
+
+            for (int i = N-1, k=0; i >= (N/2)+1; i--, k++) {
+                for (int j = (N/2)+1; j <= (N/2)+k; j++) {
+                    sum += arr[i][j];
+                }
+                for (int j = (N/2); j >= (N/2)-k; j--) {
+                    sum += arr[i][j];
+                }
+            }
+
+            System.out.println("#" + test_case + " " + sum);
+        }
+    }
+}
