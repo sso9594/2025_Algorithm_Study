179b4ab385150aa9d18c5f1ab0bba087a4339e83
sso9594 <sso9594@naver.com>
2025-03-06 15:34:44 +0900

    [Baekjoon-15650]: N과 M 2

---
+++ seungyong/Baekjoon/N과M2_15650.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class N과M2_15650 {
+    static ArrayList<int[]> permuSet;
+    static int N;
+    static int M;
+    static int[] arr;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        arr = new int[N];
+        permuSet = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i+1;
+        }
+
+        permu(0, 0, new int[M], new boolean[N]);
+
+        for (int[] set : permuSet) {
+            for (int i = 0; i < set.length; i++) {
+                System.out.print(set[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void permu(int index, int depth, int[] sel, boolean[] visited){
+        if(depth==M){
+            permuSet.add(Arrays.copyOf(sel, depth));
+            return;
+        }
+
+        for (int i = index; i < N; i++) {
+            if(!visited[i]){
+                visited[i] = true;
+                sel[depth] = arr[i];
+                permu(i+1, depth+1, sel, visited);
+                visited[i] = false;
+            }
+        }
+    }
+}
