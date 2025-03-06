b5d987e7c52819290801bd7f8cc217e2e24767c1
sso9594 <sso9594@naver.com>
2025-03-06 15:35:06 +0900

    [Baekjoon-15651]: N과 M 3

---
+++ seungyong/Baekjoon/N과M3_15651.java
@@ -0,0 +1,53 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class N과M3_15651 {
+    static ArrayList<int[]> permuSet;
+    static int N;
+    static int M;
+    static int[] arr;
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        arr = new int[N];
+        permuSet = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i+1;
+        }
+
+        permu(0, 0, new int[M], new boolean[N]);
+
+        for (int[] set : permuSet) {
+            for (int i = 0; i < set.length; i++) {
+                bw.write(set[i] + " ");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+    }
+
+    static void permu(int index, int depth, int[] sel, boolean[] visited){
+        if(depth==M){
+            permuSet.add(Arrays.copyOf(sel, depth));
+            return;
+        }
+        
+        if(index==N){
+            return;
+        }
+        
+        for (int i = 0; i < N; i++) {
+            sel[depth] = arr[i];
+            permu(index, depth+1, sel, visited);
+        }
+    }
+}
